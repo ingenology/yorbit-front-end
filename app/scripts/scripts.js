1ed92dd4bb266acc65ee969919c7ba6e9b29a813
Doug Niccum <dniccum@ingenology.com>
2014-05-22 17:09:45 -0500

    Initial Commit

---
+++ app/scripts/scripts.js
@@ -0,0 +1,202 @@
+'use strict';
+
+// GLOBAL VARIABLES
+var date;
+
+//NAVIGATION
+var triggerBttn = $( '#trigger-overlay' ),
+	overlay = $( '.overlay' ),
+	closeBttn = $( '.overlay-close' );
+
+function toggleOverlay() {
+	if (overlay.hasClass('open') ) {
+		overlay.removeClass('open');
+		overlay.addClass('close');
+	}
+	if ( !overlay.hasClass('close') ) {
+		overlay.addClass('open');
+	}
+}
+
+function overlayReset() {
+	overlay.removeClass('close');
+}
+
+triggerBttn.click(function() {
+	toggleOverlay();
+});
+closeBttn.click(function() {
+	toggleOverlay();
+	setTimeout(overlayReset, 1000);
+});
+//overlay.delay(1000).removeClass('close');
+
+// DATE PICKER
+$(function() {
+	$( '#dateIn' ).datepicker({
+		'dateFormat': 'yy-mm-dd'
+	});
+});
+
+$('.map-type .button').click(function() {
+	var data = $(this).attr('data');
+	$('#mapType').val(data);
+	$(this).parent().find('.button').removeClass('active');
+	$(this).addClass('active');
+});
+
+$('#modal1 .btn').click(function() {
+	var value = $('#dateIn').val();
+	var fullDate = new Date();
+	var twoDigitMonth = ((fullDate.getMonth().length+1) === 1)? (fullDate.getMonth()+1) : '0' + (fullDate.getMonth()+1);
+	
+	var dateStripped = fullDate.getFullYear() + twoDigitMonth + fullDate.getDate();
+	var valueStripped = value.replace(/\-/g,'');
+
+	if (value === '') {
+		alert('Hey, select a date.');
+	} else if (valueStripped >= dateStripped) {
+		alert('Hey, select a date that is not in the future.');
+	} else {
+		/*
+		date = value;
+		$('#modal1').animate({
+			top: '-50%'
+		}, 600, 'easeOutQuint', function() {
+			$(this).hide(function() {
+				$('#modal2').show();
+				$('#modal2').animate({
+					top: '18%',
+					opacity: '1'
+				}, 600, 'easeOutQuint');
+			});
+		});
+		*/
+	}
+});
+
+// APP SCRIPTS
+$('.application .tools .item.type .button').click(function() {
+	//var data = $(this).attr('data');
+	$(this).parent().find('.button').removeClass('active');
+	$(this).addClass('active');
+});
+
+// TYPE SIZE SLIDER
+$(function() {
+    $('.application .tools .item.type .bar_slider').slider({
+        value: 60,
+        min: 8,
+        max: 120,
+        step: 2,
+        range: 'min',
+        slide: function( event, ui ) {
+            var val = ui.value + 'px';
+            $('#messageSpan').css('font-size', val);
+            $('#textSize').val(ui.value);
+        }
+    });
+});
+
+//ACCORDIAN
+$('.application .tools .item .title').click(function() {
+	var parent = $(this).parent();
+	if ( parent.hasClass('active') ) {
+		parent.removeClass('active');
+	} else {
+		//$(this).parent().parent().find('.item').removeClass('active');
+		parent.addClass('active');
+	}
+});
+
+//OBJECT SLIDERS
+$(function() {
+    $('.application .tools .item.objects .bar_sliderW').slider({
+        value: 100,
+        min: 50,
+        max: 600,
+        step: 1,
+        range: 'min',
+        slide: function( event, ui ) {
+            var val = ui.value + 'px';
+            $('#messageDiv').css('width', val);
+            $('#messageWidth').val(ui.value);
+
+        }
+    });
+});
+ 
+ 
+ 
+$(function() {
+    $('.application .tools .item.objects .bar_sliderH').slider({
+        value: 50,
+        min: 50,
+        max: 600,
+        step: 1,
+        range: 'min',
+        slide: function( event, ui ) {
+            var val = ui.value + 'px';
+            $('#messageDiv').css('height', val);
+            $('#messageHeight').val(ui.value);
+        }
+    });
+});
+ 
+$(function() {
+    $('.application .tools .item.objects .bar_sliderO').slider({
+        value: 1,
+        min: 0,
+        max: 1,
+        step: '.1',
+        range: 'min',
+        slide: function( event, ui ) {
+            var val = ui.value;
+            $('#messageDiv').css('opacity', val);
+            $('#messageOpacity').val(val);
+        }
+    });
+});
+ 
+$(function() {
+    $('.application .tools .item.objects .bar_sliderB').slider({
+        value: 10,
+        min: 0,
+        max: 300,
+        step: 1,
+        range: 'min',
+        slide: function( event, ui ) {
+            var val = ui.value + 'px';
+            $('#messageDiv').css('border-radius', val);
+            $('#borderRad').val(ui.value);
+ 
+        }
+    });
+});
+
+//PATTERN SLIDERS
+$(function() {
+    $('.application .tools .item.patterns .bar_slider').slider({
+		value: 50,
+		min: 1,
+		max: 100,
+		step: 1,
+		range: 'min',
+		slide: function( event, ui ) {
+			
+		}
+    });
+});
+
+// SHARE OVERLAY
+$('#share-now').click(function() {
+	$('.share-overlay').fadeIn(300);
+
+	return false;
+});
+
+$('.addthis_toolbox a, .close_navigation').click(function() {
+	$('.share-overlay').fadeOut(300);
+
+	return false;
+});
